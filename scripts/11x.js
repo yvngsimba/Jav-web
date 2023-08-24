2f3aee9bcd201f78ae58667483d456d43993c967
yvngsimba <52266231+yvngsimba@users.noreply.github.com>
2023-08-24 04:52:07 -0400

    adding local storage to todo list application

---
+++ scripts/11x.js
@@ -0,0 +1,62 @@
+let todoList = [{
+  name: 'Make dinner',
+  dueDate: '2022-12-22'
+}, {
+  name: 'Wash dishes',
+  dueDate: '2022-12-23'
+}];
+
+let storedData = localStorage.getItem('todoList');
+
+if (storedData) {
+  todoList = JSON.parse(storedData);
+}
+
+renderTodoList();
+function renderTodoList() {
+  let todoListHTML = '';
+
+  for (let i = 0; i < todoList.length; i++) {
+    const todoObject = todoList[i];
+    const { name, dueDate } = todoObject;
+    const html =`
+      <p>
+        ${name}
+      </p>
+      <p>
+        ${dueDate}
+      </p>
+      <p>
+        <button class="delete-button" onclick="
+          todoList.splice(${i}, 1)
+          renderTodoList()
+          localStorage.setItem('todoList', JSON.stringify(todoList));
+        ">Delete</button>
+      </p>
+    `;
+    todoListHTML += html; 
+  }
+
+  const divElement = document.querySelector('.todo-grid');
+  divElement.innerHTML = todoListHTML;
+}
+
+function addTodo() {
+  const inputElement = document.querySelector('.js-todo');
+  const inputDuedate = document.querySelector('.js-duedate');
+  
+
+  const name = inputElement.value;
+  const dueDate = inputDuedate.value;
+  
+  todoList.push({
+    name,
+    dueDate
+  });
+
+  inputElement.value = '';
+
+ 
+  renderTodoList();
+  localStorage.setItem('todoList', JSON.stringify(todoList));
+}
