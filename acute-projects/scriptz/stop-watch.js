cd74b39b0057455e3039d88eb26d17ff7711ebce
yvngsimba <52266231+yvngsimba@users.noreply.github.com>
2023-09-03 04:49:58 -0400

    Stopwatch App completed

--- acute-projects/scriptz/stop-watch.js
+++ acute-projects/scriptz/stop-watch.js
@@ -1,24 +1,61 @@
 let seconds = 0;
 let minutes = 0;
 let hours = 0;
+let timerRunning = false;
+let timerInterval;
+
 
 const label = document.querySelector('p');
 
-label.innerHTML = `${hours}:${minutes}:${seconds}`
 
-function timeTracker() {
- 
+
+function startTimer() {
   
-  setInterval(() => {
-    seconds++;
-  }, 1000);
+  if (!timerRunning) {
+    timerInterval = setInterval(updateTimer, 1000);
+    timerRunning = true;
+  }
+}
+
+function stopTimer() {
+  clearInterval(timerInterval);
+  timerRunning = false;
+}
+
+function resetTimer() {
+  clearInterval(timerInterval);
+  timerRunning = false;
+  seconds = 0;
+  minutes = 0;
+  hours = 0;
+  label.innerHTML = `${hours}:${minutes}:${seconds}` 
+}
+
+function updateTimer() {
+  seconds++;
+  
+  if (seconds === 60) {
+      seconds = 0;
+      minutes++;
+  } else if (minutes === 60) {
+      minutes = 0;
+      hours++;
+  }
+
+  label.innerHTML = `${hours}:${minutes}:${seconds}`
 }
 
 document.querySelector('.js-start')
   .addEventListener('click',() => {
-    setInterval(() => {
-      seconds++;
-    }, 1000);
+    startTimer();
 });
 
-console.log(seconds);
+document.querySelector('.js-stop')
+  .addEventListener('click',() => {
+    stopTimer();
+});
+
+document.querySelector('.js-reset')
+  .addEventListener('click',() => {
+    resetTimer();
+});
